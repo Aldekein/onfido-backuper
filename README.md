c4f7a82814e39aa5c3fdfb76b554341e6f787084
Oleh Aldekein <i.am@aldeke.in>
2019-12-13 21:57:52 +0700

    Added caching to improve processing on lots of data

--- README.md
+++ README.md
@@ -1,12 +1,13 @@
 # onfido-backuper
-This tool will create a backup of all data in your Onfido account
+This tool will create a backup of most important user data in your Onfido account
 
 # Usage
 * Create `config/onfido_token.txt` file and past your API token there
-* Run `node index.js` to make the backup
+* User `create_db_tables.sql` to create required tables in MySQL database
+* Run `node index.js init`, `node index.js cache`, `node index.js download` to make the backup
 * Applicants' KYC data and documents from your account will be downloaded to a `data` directory
 * Make sure you strongly encrypt this data before storage, since there's lots of PII!
 
 # Not planned, but possible improvements
-* Support other Onfido data that may exist in account (checks, videos, live photos etc)
-* Make it more suitable for automated update (pass secrets via ENV, improve logging)
+* Support other Onfido data that may exist in account (videos, live photos etc)
+* Make it more suitable for automated backups (pass secrets via ENV, improve logging)
--- index.js
+++ index.js
@@ -8,7 +8,16 @@ var fs = require('fs');
 const throttle = require('promise-ratelimit')(200);
 const recordsPerPage = 100;
 const applicantsDataPath = 'data/applicants.json';
-const processedDataPath = 'data/processed.json';
+
+// init mysql to be used in script for cache, see create_db_table.sql for model
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'onfido'
+});
+connection.connect();
 
 // no need to configure below, initialization
 const secret = fs.readFileSync('config/onfido_token.txt', 'utf8');
@@ -17,29 +26,44 @@ const tokenAuth = defaultClient.authentications['Token'];
 tokenAuth.apiKey = 'token='+secret;
 tokenAuth.apiKeyPrefix = 'Token';
 const api = new Onfido.DefaultApi();
-var markedAsProcessed = [];
 
 (async () => {
-  // get all applicants you’ve created
-  var allApplicants;
-  if (!fs.existsSync('data')) fs.mkdirSync('data');
-  if (fs.existsSync(processedDataPath)) {
-    markedAsProcessed = JSON.parse(fs.readFileSync(processedDataPath, 'utf8'));
+  if (process.argv[2] == 'init') {
+      // get all applicants you’ve created
+      if (!fs.existsSync('data')) fs.mkdirSync('data');
+      var allApplicants = await listAllApplicants();
+      fs.writeFileSync(applicantsDataPath, JSON.stringify(allApplicants, null, 2));
+      console.log('Updated applicants data saved to ' + applicantsDataPath);
+      connection.end();
   }
-  if (fs.existsSync(applicantsDataPath)) {
-    allApplicants = JSON.parse(fs.readFileSync(applicantsDataPath, 'utf8'));
+  else if (process.argv[2] == 'cache') {
+      if (!fs.existsSync('data') || !fs.existsSync(applicantsDataPath)) {
+        console.log('Please run: node index.js init');
+      }
+
+      // get all applicants you’ve created
+      var allApplicants = JSON.parse(fs.readFileSync(applicantsDataPath, 'utf8'));
+      console.log(`${allApplicants.length} total applicants to be processed.`);
+
+      await allApplicants.forEach(entry => cacheApplicantData(entry));
+      await allApplicants.forEach(entry => cacheApplicantChecks(entry.id));
+
+      // get all documents for every applicant and save them to disk
+      await allApplicants.forEach(entry => cacheApplicantDocuments(entry.id));
+      
+      // connection.end();
+  }
+  else if (process.argv[2] == 'download') {
+    var query = connection.query('SELECT * FROM files WHERE status=0', [], async function (error, results, fields) {
+      if (error) throw error;
+      await results.forEach(document => saveApplicantDocument(document));
+      // connection.end();
+    });
   }
   else {
-    allApplicants = await listAllApplicants();
-    fs.writeFileSync(applicantsDataPath, JSON.stringify(allApplicants, null, 2));
-    console.log('Updated applicants data saved to ' + applicantsDataPath);
+    console.log('Call me like: node index.js init or node index.js download')
   }
-  console.log(`${allApplicants.length} total applicants to be processed.`);
-
-  // get all documents for every applicant and save them to disk
-  allApplicants.forEach(entry => saveApplicantDocuments(entry.id));
 })();
-
 
 async function listAllApplicants() {
   try {
@@ -64,33 +88,97 @@ async function listAllApplicants() {
   }
 }
 
-async function saveApplicantDocuments(applicantId) {
-  if (!markedAsProcessed.includes(applicantId)) {
-    try {
-      await throttle();
-      console.log(`Requesting documents for applicant ${applicantId}...`);
-      const { documents } = await api.listDocuments(applicantId);
-      
-      const path = 'data/'+applicantId;
-      if (!fs.existsSync(path)) fs.mkdirSync(path);
-      documents.forEach(document => saveApplicantDocument(applicantId, document));
-    } catch(e) {
-      console.log('ERROR CAUGHT:');
-      console.log(e);
-    }
+async function cacheApplicantData(applicant) {
+  try {
+    const recordData = {
+      applicantId: applicant.id,
+      data: JSON.stringify(applicant)
+    };
+    connection.query('INSERT INTO applicants SET ? ON DUPLICATE KEY UPDATE applicantId=applicantId', recordData, function (error, results, fields) {
+      if (error) throw error;
+    });
+  } catch(e) {
+    console.log('DB ERROR CAUGHT:');
+    console.log(e);
   }
 }
 
-async function saveApplicantDocument(applicantId, document) {
+async function cacheApplicantDocuments(applicantId) {
   try {
-    const documentId = document.id, 
-          docType = document.type, 
-          fileType = document.file_type, 
-          fileSize = document.file_size,
+    await throttle();
+    console.log(`Requesting documents for applicant ${applicantId}...`);
+    const { documents } = await api.listDocuments(applicantId);
+
+    connection.query('UPDATE applicants SET documentCount=? WHERE applicantId=?', [documents.length, applicantId], function (error, results, fields) {
+      console.log(`Expecting to download ${documents.length} documents for applicant ${applicantId}.`);	
+    });
+    
+    const path = 'data/'+applicantId;
+    if (!fs.existsSync(path)) fs.mkdirSync(path);
+    documents.forEach(document => cacheApplicantDocument(applicantId, document));
+  } catch(e) {
+    console.log('ERROR CAUGHT:');
+    console.log(e);
+  }
+}
+
+async function cacheApplicantChecks(applicantId) {
+  try {
+    await throttle();
+    console.log(`Requesting checks for applicant ${applicantId}...`);
+
+    var checks = await api.apiClient.callApi(
+      '/applicants/{applicant_id}/checks', 'GET',
+      {'applicant_id': applicantId}, {expand: 'reports'}, {}, {}, {}, null,
+      ['Token'], [], ['application/json'], Onfido.ChecksList
+    );
+
+    connection.query('INSERT INTO checks SET ? ON DUPLICATE KEY UPDATE applicantId=applicantId', [{applicantId: applicantId, data: checks.response.res.text}], function (error, results, fields) {
+      if (error) throw error;
+    });
+  } catch(e) {
+    console.log('ERROR CAUGHT:');
+    console.log(e);
+  }
+}
+
+async function cacheApplicantDocument(applicantId, document) {
+  try {
+    const path = `data/${applicantId}/${document.type}-${document.id}.${document.file_type}`;
+    const processed = (fs.existsSync(path)) ? 1 : 0; // cannot check && fs.statSync(path)['size'] == document.file_size, Onfido returns fake size!
+    const recordData = {
+      applicantId: applicantId, 
+      documentId: document.id, 
+      docType: document.type,
+      fileType: document.file_type,
+      fileSize: document.file_size,
+      status: processed
+    };
+
+    connection.query('INSERT INTO files SET ? ON DUPLICATE KEY UPDATE status=?', [recordData, processed], function (error, results, fields) {
+      if (error) throw error;
+    });
+  } catch(e) {
+    console.log('DB ERROR CAUGHT:');
+    console.log(e);
+  }
+}
+
+async function saveApplicantDocument(document) {
+  try {
+    const applicantId = document.applicantId,
+          documentId = document.documentId, 
+          docType = document.docType, 
+          fileType = document.fileType, 
+          fileSize = document.fileSize,
+          docStatus = document.status,
           path = `data/${applicantId}/${docType}-${documentId}.${fileType}`;
 
+    const dirPath = 'data/'+applicantId;
+    if (!fs.existsSync(dirPath)) fs.mkdirSync(dirPath);
+
     // this allows to download files only once and save a lot of traffic and requests
-    if (!fs.existsSync(path) || fs.statSync(path)['size'] != fileSize) {
+    if (!fs.existsSync(path)) {
       await throttle();
       const document = await api.apiClient.callApi(
         `/applicants/${applicantId}/documents/${documentId}/download`, 'GET',
@@ -104,10 +192,13 @@ async function saveApplicantDocument(applicantId, document) {
       fs.writeFileSync(path, document.data);
       console.log(`Saved document ${docType} ${documentId} for applicant ${applicantId}`);
     }
-    else {
-      markedAsProcessed.push(applicantId);
-      // that sucks, neet to do once:
-      fs.writeFileSync(processedDataPath, JSON.stringify(markedAsProcessed, null, 2));
+    if (docStatus == 0) {
+      var query = connection.query('UPDATE files SET status=1 WHERE applicantId=? and documentId=?', [ applicantId, documentId ], function (error, results, fields) {
+        if (error) {
+          console.log(query.sql);
+          throw error;
+        }
+      });
     }
   } catch(e) {
     console.log('ERROR CAUGHT:');
